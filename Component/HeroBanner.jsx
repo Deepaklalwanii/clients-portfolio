54e4fa6c7dd92b38e561dae4bfe9bd1eead4464c
deepaklalwanii <deepaklalwani752@gmail.com>
2025-03-21 16:41:14 +0530

    init

--- Component/HeroBanner.jsx
+++ Component/HeroBanner.jsx
@@ -5,7 +5,7 @@ const HeroBanner = () => {
     <>
     <div className='herobanner-wrapper'>
         <p>I am Vaishali Jain, a pistol shooter specializing in the 10m Air Pistol event. I have competed in multiple state and national championships, including Indian Team Selection Trials, and have won medals in Khelo India and State Championships. Currently training at Gun for Glory Academy, I am also pursuing a degree in Psychology while working towards my goal of representing India at the international level.</p>
-        <img src="src\assets\0.jpg" alt="shooting-profile" />
+        <img src="src/images/banner.jpg" alt="shooting-profile" />
     </div>
     </>
   )
